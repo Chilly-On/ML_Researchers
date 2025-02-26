f54b3186a8c50a326e875d79973dc5022c3e91b0
utbin183 <150402160+utbin183@users.noreply.github.com>
2025-02-26 12:06:40 +0700

    Add code data

---
+++ data/ted_talk_es.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import sys
+
+# Sửa lỗi in tiếng Việt trong terminal (Windows)
+sys.stdout.reconfigure(encoding="utf-8")
+
+# Đường dẫn file CSV
+file_path = r"d:\btl ai\ML_Researchers-main\data\raw\ted_talks_es.csv"
+
+# Đọc file CSV với UTF-8 và giữ lại các cột cần thiết
+df = pd.read_csv(file_path, encoding="utf-8")[["transcript"]].dropna()
+
+
+df.to_csv("transcripts_es.csv", index=False)
